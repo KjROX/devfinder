12e52e28fcdeb2e59ad1262bd64b20bd69cb4e76
SankhaChak <anonproductionspro@gmail.com>
2022-05-14 15:41:06 +0530

    changed variable name

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const bio = document.querySelector(".user-details");
 const repos = document.querySelector(".repo-num");
 const followers = document.querySelector(".followers-num");
 const following = document.querySelector(".following-num");
-const locationName = document.querySelector(".link1 p");
-const link = document.querySelector(".link2 a");
-const twitter = document.querySelector(".link3 p");
-const company = document.querySelector(".link4 p");
-const link1 = document.querySelector(".link1");
-const link2 = document.querySelector(".link2");
-const link3 = document.querySelector(".link3");
-const link4 = document.querySelector(".link4");
+const locationName = document.querySelector(".locationEl p");
+const link = document.querySelector(".profileLinkEl a");
+const twitter = document.querySelector(".twitterProfileEl p");
+const companyName = document.querySelector(".companyEl p");
+const locationEl = document.querySelector(".locationEl");
+const profileLinkEl = document.querySelector(".profileLinkEl");
+const twitterProfileEl = document.querySelector(".twitterProfileEl");
+const companyEl = document.querySelector(".companyEl");
 const theme = document.querySelector("header div");
 const themeName = theme.querySelector("h2");
 const themeImg = theme.querySelector("img");
@@ -116,33 +116,33 @@ function handleResponse(data) {
   repos.textContent = data.public_repos;
   if (data.location) {
     locationName.textContent = data.location;
-    checkANDremove(locationName, link1);
+    checkANDremove(locationName, locationEl);
   } else {
-    add(locationName, link1);
+    add(locationName, locationEl);
     locationName.textContent = "NOT AVAILABLE";
   }
   if (data.blog) {
     link.textContent = data.blog;
     link.href = data.blog;
-    checkANDremove(link, link2);
+    checkANDremove(link, profileLinkEl);
   } else {
     link.href = "#";
-    add(link, link2);
+    add(link, profileLinkEl);
     link.textContent = "NOT AVAILABLE";
   }
   if (data.twitter_username) {
     twitter.textContent = data.twitter_username;
-    checkANDremove(twitter, link3);
+    checkANDremove(twitter, twitterProfileEl);
   } else {
-    add(twitter, link3);
+    add(twitter, twitterProfileEl);
     twitter.textContent = "NOT AVAILABLE";
   }
   if (data.company) {
-    company.textContent = data.company;
-    checkANDremove(company, link4);
+    companyName.textContent = data.company;
+    checkANDremove(companyName, companyEl);
   } else {
-    add(company, link4);
-    company.textContent = "NOT AVAILABLE";
+    add(companyName, companyEl);
+    companyName.textContent = "NOT AVAILABLE";
   }
 }
 async function searchInfo(search) {
